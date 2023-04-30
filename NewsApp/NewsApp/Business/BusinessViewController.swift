cfaf0b5040d9c31e859a76dc3014528130c08a41
Marat Cherkasov <marat.cherkasov@yandex.ru>
2023-04-30 20:26:46 +0500

    lesson 2

--- NewsApp/NewsApp/Business/BusinessViewController.swift
+++ NewsApp/NewsApp/Business/BusinessViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 final class BusinessViewController: UIViewController {
     
+    // MARK: - GUI Variables
+    
+    // MARK: - Properties
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .green
     }
+    
+    // MARK: - Methods
+    
+    // MARK: - Private methods
 }
--- NewsApp/NewsApp/Technology/TechnologyViewController.swift
+++ NewsApp/NewsApp/Technology/TechnologyViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 final class TechnologyViewController: UIViewController {
+    // MARK: - GUI Variables
     
+    // MARK: - Properties
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .link
     }
+    
+    // MARK: - Methods
+    
+    // MARK: - Private methods
 }
